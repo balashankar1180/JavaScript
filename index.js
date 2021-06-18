c9a31871f05ec95f510a5a67437538e566acf894
balashankar1180 <balashankar1180@gmail.com>
2021-06-19 02:54:33 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Import stylesheets
 import './style.css';
-
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Balashankar Gujja</h1>`;
